944b87d9107fd8a388603cc2d74996ad15ae2604
zongying.cao <zongying.cao@dxc.com>
2018-12-15 03:39:04 +0000

    add test.py
    Signed-off-by: zongying.cao <zongying.cao@dxc.com>

--- test.py
+++ test.py
@@ -7,6 +7,16 @@ def main():
     
     lg.debug("debug hello to cat1")
     lg.info("info hello to cat1")
+<<<<<<< 6977a306e12a0c87acbe811729f075e7963a6495
+=======
+
+    def greet_me(**kwargs):
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                print("%s == %s" %(key,value))
+
+    greet_me(name="alan", id=123)
+>>>>>>> add test
     
 if __name__ == "__main__":
     main()
